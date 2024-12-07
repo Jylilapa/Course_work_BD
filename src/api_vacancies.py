a135542b468c433057f61b38e30ea0bc70c650d3
jylu lyapunova <jylilapa@yandex.ru>
2024-12-07 18:10:41 +0300

    add functional

--- src/api_vacancies.py
+++ src/api_vacancies.py
@@ -1,30 +1,55 @@
 import requests
-from abc import ABC, abstractmethod
 
 
-class ApiVacancies(ABC):
-    """Абстрактный класс получения вакансий по API"""
+class HHParser:
 
-    @abstractmethod
-    def load_vacancies(self, keyword):
-        pass
+    @staticmethod
+    def __get_response():
+        """получает ответ от HH"""
+        params = {"sort_by": "by_vacancies_open"}
+        response = requests.get("https://api.hh.ru/employers", params=params)
+        if response.status_code == 200:
+            return response.json()["items"]
 
+    def get_employers(self):
+        """получает работодателей"""
+        data = self.__get_response()
+        employers = []
+        for employer in data:
+            employers.append({"id": employer["id"], "name": employer["name"]})
+        return employers
 
-class HeadHunter(ApiVacancies):
-    """Класс получения вакансий с сайта HeadHunter"""
+    def get_vacancies(self):
+        """получает вакансии"""
+        employers = self.get_employers()
+        vacancies = []
+        for employer in employers:
+            params = {"employer_id": employer["id"]}
+            response = requests.get("https://api.hh.ru/vacancies", params=params)
+            if response.status_code == 200:
+                filtered_vacancies = self.__filter_vacancies(response.json()["items"])
+                vacancies.extend(filtered_vacancies)
+        return vacancies
 
-    def __init__(self):
-        self.url = "https://api.hh.ru/vacancies"
-        self.headers = {"User-Agent": "HH-User-Agent"}
-        self.params = {"text": "", "page": 0, "per_page": 100}
-        self.vacancies = []
-
-    def load_vacancies(self, keyword):
-        """Метод получения вакансий"""
-        self.params["text"] = keyword
-        while self.params.get("page") != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
-            vacancies = response.json()["items"]
-            self.vacancies.extend(vacancies)
-            self.params["page"] += 1
-            return vacancies
+    @staticmethod
+    def __filter_vacancies(vacancies):
+        """фильтрует вакансии"""
+        filtered_vacancies = []
+        for vacancy in vacancies:
+            if vacancy["salary"] is None:
+                salary_from = 0
+                salary_to = 0
+            else:
+                salary_from = vacancy["salary"]["from"] if vacancy["salary"]["from"] else 0
+                salary_to = vacancy["salary"]["to"] if vacancy["salary"]["to"] else 0
+            filtered_vacancies.append(
+                {
+                    "id": vacancy["id"],
+                    "name": vacancy["name"],
+                    "link": vacancy["alternate_url"],
+                    "salary_from": salary_from,
+                    "salary_to": salary_to,
+                    "employer": vacancy["employer"]["id"],
+                }
+            )
+        return filtered_vacancies
